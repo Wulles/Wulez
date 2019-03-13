38a79380a72e99fd1d1a9a6d8aa7d991621c2774
Wulles <lhdsa@icloud.com>
2019-03-13 12:49:37 -0300

    Base r1
    Normal files

---
+++ English/catalog_featured_pages.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `catalog_featured_pages` (
+  `slot_id` int(11) NOT NULL,
+  `image` varchar(64) NOT NULL DEFAULT '',
+  `caption` varchar(128) NOT NULL DEFAULT '',
+  `type` enum('page_name','page_id','product_name') NOT NULL DEFAULT 'page_name',
+  `expire_timestamp` int(11) NOT NULL DEFAULT '-1',
+  `page_name` varchar(16) NOT NULL DEFAULT '',
+  `page_id` int(11) NOT NULL DEFAULT '0',
+  `product_name` varchar(32) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO `catalog_featured_pages` (`slot_id`, `image`, `caption`, `type`, `expire_timestamp`, `page_name`, `page_id`, `product_name`) VALUES
+(1, 'catalogue/feature_cata/feature_cata_hort_HC_b.png', 'Seja HC!', 'page_name', -1, 'behcbitch', 912422, ''),
+(2, 'catalogue/feature_cata/feature_cata_hort_clothes.png', 'Loja de Roupas', 'page_name', -1, 'clothingspage', 912454, ''),
+(3, 'catalogue/feature_cata/feature_cata_hort_house18_bun2.png', 'Gamer Pro', 'page_name', -1, 'gamingc18', 9125240, ''),
+(4, 'catalogue/feature_cata/feature_cata_vert_diapaintings18.png', 'NOVAS Obras de Arte Diamante!', 'page_name', -1, 'wallfurnisbitch', 912447, '');
---
+++ Portuguese/catalog_club_offers.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `catalog_club_offers` (
+  `id` int(11) NOT NULL,
+  `enabled` enum('0','1') NOT NULL DEFAULT '1',
+  `name` varchar(35) NOT NULL,
+  `days` int(7) NOT NULL,
+  `credits` int(5) NOT NULL DEFAULT '10',
+  `points` int(5) NOT NULL DEFAULT '0',
+  `points_type` int(3) NOT NULL DEFAULT '0',
+  `type` enum('HC','VIP') NOT NULL DEFAULT 'HC',
+  `deal` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `catalog_club_offers` (`id`, `enabled`, `name`, `days`, `credits`, `points`, `points_type`, `type`, `deal`) VALUES
+(1, '1', 'HABBO_CLUB_1_MONTH', 31, 50, 50, 5, 'VIP', '0'),
+(3, '1', 'HABBO_CLUB_3_MONTH', 90, 120, 120, 5, 'VIP', '0');
