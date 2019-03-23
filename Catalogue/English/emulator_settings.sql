8193471e38f15175e54049ade3492b2ff002f0f7
Wulles <lhdsa@icloud.com>
2019-03-23 19:11:49 -0300

    emulator_settings

--- Catalogue/English/emulator_settings.sql
+++ Catalogue/English/emulator_settings.sql
@@ -1,2 +1,7 @@
 UPDATE `emulator_settings` SET `value` = '1' WHERE `emulator_settings`.`key` = 'hotel.catalog.purchase.cooldown';
 UPDATE `emulator_settings` SET `value` = '1' WHERE `emulator_settings`.`key` = 'hotel.catalog.items.display.ordernum';
+UPDATE `emulator_settings` SET `value` = '1' WHERE `emulator_settings`.`key` = 'wired.effect.teleport.delay';
+UPDATE `emulator_settings` SET `value` = '912481' WHERE `emulator_settings`.`key` = 'catalog.page.vipgifts';
+UPDATE `emulator_settings` SET `value` = '4590' WHERE `emulator_settings`.`key` = 'camera.item_id';
+UPDATE `emulator_settings` SET `value` = '10' WHERE `emulator_settings`.`key` = 'catalog.guild.price';
+UPDATE `emulator_settings` SET `value` = '230' WHERE `emulator_settings`.`key` = 'catalog.ltd.page.soldout';
