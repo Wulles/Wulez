c025ffd4b1558ca9a10d11184c0b59c9de1f34b9
Wulles <lhdsa@icloud.com>
2019-04-04 13:26:37 -0300

    Hween 18 Craft
    With achievements working.

--- Catalogue/English/crafting_altars_recipes.sql
+++ Catalogue/English/crafting_altars_recipes.sql
@@ -18,7 +18,15 @@ CREATE TABLE `crafting_altars_recipes` (
 
 INSERT INTO `crafting_altars_recipes` (`id`, `altar_id`, `recipe_id`) VALUES
 (1, 4310020, 1),
-(2, 4310020, 2);
+(2, 4310020, 2),
+(3, 4310020, 3),
+(4, 4310020, 4),
+(5, 4310020, 5),
+(6, 4310020, 6),
+(7, 4310020, 7),
+(8, 4310020, 8),
+(9, 4310020, 9),
+(10, 4310020, 10);
 
 ALTER TABLE `crafting_altars_recipes`
   ADD PRIMARY KEY (`id`);
