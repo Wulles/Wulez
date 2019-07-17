664a589ca9e08a00ef640ef855aea18892909ff3
Wulles <lhdsa@icloud.com>
2019-07-17 13:18:18 -0300

    Talking furnis. (1/3)
    Holiday Santa Fortune Teller.

---
+++ English/emulator_texts.sql
@@ -0,0 +1,13 @@
+INSERT INTO `emulator_texts` (`key`, `value`) VALUES
+('xmas14_santateller.message.1', 'Did you know i have a zen filled holiday season.'),
+('xmas14_santateller.message.2', 'Everyday is a surprise.'),
+('xmas14_santateller.message.3', 'All i want for christmas is cookies. tons of cookies.'),
+('xmas14_santateller.message.4', 'Inner peace starts with an hc subscription.'),
+('xmas14_santateller.message.5', 'Dont get your tinsel in a tangle.'),
+('xmas14_santateller.message.6', 'What do snowmen eat for lunch? ice bergers.'),
+('xmas14_santateller.message.7', 'I heard rudolph stole that nose from a Wulles clown.'),
+('xmas14_santateller.message.8', 'Ho ho ho-ohmmmmm.'),
+('xmas14_santateller.message.9', 'Or is that just my eyes?!This seashell is whispering sweet nothings in my ear. oh, its got a crab inside!'),
+('xmas14_santateller.message.10', 'Be the builders block.'),
+('xmas14_santateller.message.11', 'What if life isnt all about whos around it.'),
+('xmas14_santateller.message.12', 'Its not about whats under the tree, its about whos around it.');
